fe57c541609906033b78e6c65aca1c6da911dce1
Zahra Karandish <zkarandish@gmail.com>
2025-01-29 06:33:51 -0500

    server side validation

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {body} = require('express-validator');
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
@@ -6,6 +7,13 @@ const router = express.Router();
 // GET/ feed/posts
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost);
+router.post('/post',[
+    body('title')
+    .trim()
+    .isLength({min:5}),
+    body('content')
+    .trim()
+    .isLength({min:5})
+] ,feedController.createPost);
 
 module.exports = router;
