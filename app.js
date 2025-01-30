659fbcc22507cb066a200e2fcb07cd7e69bb25b0
Zahra Karandish <zkarandish@gmail.com>
2025-01-30 06:48:06 -0500

    file upload

--- app.js
+++ app.js
@@ -2,14 +2,36 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage= multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,'images');
+    },
+    filename: (req,file,cb)=>{
+        cb(null,uuidv4());
+    }
+    
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
 
 app.use(bodyParser.json()); //aplication/json
+app.use(multer({storage:fileStorage, fileFilter:fileFilter}).single('image'));
 app.use('/images', express.static(path.join(__dirname,'images')));
 
 app.use((req,res,next)=>{
@@ -25,8 +47,7 @@ app.use((error, req, res, next)=>{
     const status = error.statusCode || 500;
     const message = error.message;
     res.status(status).json({message:message});
-
-})
+});
 
 mongoose.connect('mongodb+srv://zkarandish:kGDlY2bBPkkh3BUR@cluster0.hskal.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
 .then(result=>{
