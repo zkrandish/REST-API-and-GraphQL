e28f78a643f393b847afa7360ad1858c09adf0a8
Zahra Karandish <zkarandish@gmail.com>
2025-01-30 06:03:34 -0500

    get a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,16 @@
 const {validationResult} = require('express-validator');
 const Post = require('../models/post');
 
-exports.getPosts = (req, res, nex)=>{
-    res.status(200).json({
-        posts:[{
-            _id: '1',
-            title: 'First Post', 
-            content: 'This is the first post!',
-            imageUrl:'images/boat.png',
-            creator:{
-                name: 'zara'
-            },
-            createdAt: new Date(),
-        }]
+exports.getPosts = (req, res, next)=>{
+    Post.find()
+    .then(posts=>{
+        res.status(200).json({message: 'Fetched posts successfully.',posts:posts})
+    })
+    .catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
     });
 };
 
@@ -45,6 +43,23 @@ exports.createPost = (req, res, next) => {
         }
         next(err);
     });
-
-   
 };
+
+exports.getPost = (req,res,next)=>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post=>{
+        if(!post){
+            const error = new Error('Could not find post');
+            error.statusCode= 404;
+            throw error;
+        }
+        res.status(200).json({message: 'post fetched', post:post})
+    })
+    .catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
