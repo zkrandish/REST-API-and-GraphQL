8f426afae5c80c381967d0b37e4f7cd8d02ee6d5
Zahra Karandish <zkarandish@gmail.com>
2025-02-04 15:41:33 -0500

    single post

--- app.js
+++ app.js
@@ -60,12 +60,14 @@ app.use((req, res, next) => {
     if (!req.file) {
       return res.status(200).json({ message: 'No file provided!' });
     }
-    if (req.body.oldPath) {
-      clearImage(req.body.oldPath);
+    let oldPath = req.body.oldPath;
+    if (oldPath) {
+        oldPath = oldPath.replace("\\", "/"); // ✅ Prevent Windows path issues
+        clearImage(oldPath);
     }
     return res
       .status(201)
-      .json({ message: 'File stored.', filePath: req.file.path });
+      .json({ message: 'File stored.', filePath: req.file.path.replace("\\", "/") });
   });
   
   
