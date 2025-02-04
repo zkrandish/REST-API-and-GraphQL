360368f4b47d492d95b4d2bd8364d1622e7c7dd7
Zahra Karandish <zkarandish@gmail.com>
2025-02-04 10:47:36 -0500

    connect back to front

--- app.js
+++ app.js
@@ -43,34 +43,37 @@ app.use((req, res, next) => {
       'OPTIONS, GET, POST, PUT, PATCH, DELETE'
     );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
     next();
   });
-
-  app.use('/graphql', graphqlHTTP({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err){
-        if(!err.originalError){
-            return err;
+  
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: graphqlSchema,
+      rootValue: graphqlResolver,
+      graphiql: true,
+      formatError(err) {
+        if (!err.originalError) {
+          return err;
         }
         const data = err.originalError.data;
-        const message = err.message || 'An error occured.';
+        const message = err.message || 'An error occurred.';
         const code = err.originalError.code || 500;
-        return { message: message, status: code, data:data};
-    }
-})
-);
-
-
-
-app.use((error, req, res, next)=>{
+        return { message: message, status: code, data: data };
+      }
+    })
+  );
+  
+  app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({message:message, data:data});
-});
+    res.status(status).json({ message: message, data: data });
+  });
 
 mongoose.connect('mongodb+srv://zkarandish:kGDlY2bBPkkh3BUR@cluster0.hskal.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
 .then(result => {
